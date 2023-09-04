f4c8400fb6d77c82615140dc57134f0167026e27
nadapracoloca <brunoasguimaraes@hotmail.com>
2023-09-04 18:55:49 -0300

    Commit apos alguns ajustes

--- primeiro_app/lib/main.dart
+++ primeiro_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Meu Primeiro APP de Teste',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +30,11 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 2, 138, 41)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Minha Pagina Flutter'),
     );
   }
 }
